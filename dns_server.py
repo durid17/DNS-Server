db7fbdb6cdcf4cf49ddfe8c0e6109238876d8104
giokob <giokob@gmail.com>
2019-10-11 19:34:50 +0400

    Initial commit

---
+++ dns_server.py
@@ -0,0 +1,12 @@
+import sys
+
+def run_dns_server(CONFIG, IP, PORT):
+    # your code here
+    pass
+
+# do not change!
+if __name__ == '__main__':
+    CONFIG = sys.argv[1]
+    IP = sys.argv[2]
+    PORT = sys.argv[3]
+    run_dns_server(CONFIG, IP, PORT)
